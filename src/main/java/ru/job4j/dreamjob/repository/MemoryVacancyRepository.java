0b717f0d6509c8328a59e1532d1097546c243c73
lostyway <hardsheller@gmail.com>
2025-05-17 06:48:25 +0300

    4. Thymeleaf, Циклы. [#504841 #308606]

---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -0,0 +1,55 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.util.*;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    public MemoryVacancyRepository() {
+        save(new Vacancy(0, "Intern Java Developer", "Just simple intern"));
+        save(new Vacancy(0, "Junior Java Developer", "Just simple Jun"));
+        save(new Vacancy(0, "Junior+ Java Developer", "Not a simple Jun"));
+        save(new Vacancy(0, "Middle Java Developer", "Just simple middle"));
+        save(new Vacancy(0, "Middle+ Java Developer", "not a simple middle"));
+        save(new Vacancy(0, "Senior Java Developer", "boss senior"));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        vacancies.remove(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(vacancy.getId(),
+                (key, oldVal)
+                        -> new Vacancy(oldVal.getId(), vacancy.getTitle(), vacancy.getDescription()) ) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+}
